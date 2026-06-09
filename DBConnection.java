c0a80260eb6c56e08eccc2b650dff4acd2b48798
Simeon Jauhiainen <159194709+simeonjauhiainen@users.noreply.github.com>
2026-06-09 08:31:19 -0500

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/prediction_modeldb";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Golfaddict13!";
+    private static final String USER = "username";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
